f3b53742a549693a5b1c15819fcd6f71aebaf015
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-28 17:08:39 -0300

    feat(repository): add ContractTemplateRepository

---
+++ src/main/java/dev/victor/parking/repository/ContractTemplateRepository.java
@@ -0,0 +1,7 @@
+package dev.victor.parking.repository;
+
+import dev.victor.parking.entity.ContractTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractTemplateRepository extends JpaRepository<ContractTemplate, Long> {
+}
