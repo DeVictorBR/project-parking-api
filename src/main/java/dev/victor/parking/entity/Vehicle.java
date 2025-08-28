9c30840ca48aa5eb9d899db6fa6e82113e3edc41
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-28 16:33:40 -0300

    feat(entity): add Vechicle entity

---
+++ src/main/java/dev/victor/parking/entity/Vehicle.java
@@ -0,0 +1,59 @@
+package dev.victor.parking.entity;
+
+import dev.victor.parking.entity.enums.VehicleType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_vehicles")
+public class Vehicle {
+
+    @Id
+    @Column(name = "vehicle_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(name = "license_plate", unique = true, nullable = false)
+    private String licensePlate;
+
+    @Enumerated(EnumType.STRING)
+    private VehicleType vehicleType;
+
+    public Vehicle() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+}
