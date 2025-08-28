6b0162787bc988d4bd4d59023913092e8f7a451d
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-28 16:15:28 -0300

    feat(entity): add Client entity

---
+++ src/main/java/dev/victor/parking/entity/Client.java
@@ -0,0 +1,68 @@
+package dev.victor.parking.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_clients")
+public class Client {
+
+    @Id
+    @Column(name = "client_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "cpf", unique = true)
+    private String cpf;
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
