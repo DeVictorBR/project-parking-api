6d07c562293c950f9d6573e6ff8eb89345363ab2
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-29 10:54:16 -0300

    feat(exception): add ClientNotFoundException

---
+++ src/main/java/dev/victor/parking/exception/ClientNotFoundException.java
@@ -0,0 +1,22 @@
+package dev.victor.parking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class ClientNotFoundException extends ParkingException {
+
+    private final String detail;
+
+    public ClientNotFoundException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemaDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        pd.setTitle("Client not found");
+        pd.setDetail(detail);
+        return pd;
+    }
+}
