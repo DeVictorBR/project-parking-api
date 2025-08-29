223f6ec60672eed08aa3fe096045da01e321c079
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-29 10:56:14 -0300

    feat(service): add findById method for ClientService

--- src/main/java/dev/victor/parking/service/ClientService.java
+++ src/main/java/dev/victor/parking/service/ClientService.java
@@ -2,6 +2,7 @@ package dev.victor.parking.service;
 
 import dev.victor.parking.controller.dto.ClientRequestDto;
 import dev.victor.parking.entity.Client;
+import dev.victor.parking.exception.ClientNotFoundException;
 import dev.victor.parking.repository.ClientRepository;
 import dev.victor.parking.service.dto.ClientResponseDto;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,15 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    public ClientResponseDto createClient(ClientRequestDto dto) {
+    public ClientResponseDto create(ClientRequestDto dto) {
         Client client = dto.toEntity();
         Client savedClient = clientRepository.save(client);
         return ClientResponseDto.toDto(savedClient);
+    }
+
+    public ClientResponseDto findById(Long id) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException(String.format("Client with id: %d not found", id)));
+        return ClientResponseDto.toDto(client);
     }
 }
