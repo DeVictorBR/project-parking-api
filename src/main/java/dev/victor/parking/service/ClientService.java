c14d580b4eb9a8fdfaf815e7bbc7ce61bd0cb0f6
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-29 14:48:42 -0300

    feat(service): add updatePartial method for ClientService

--- src/main/java/dev/victor/parking/service/ClientService.java
+++ src/main/java/dev/victor/parking/service/ClientService.java
@@ -1,8 +1,14 @@
 package dev.victor.parking.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import dev.victor.parking.controller.dto.ClientRequestDto;
 import dev.victor.parking.entity.Client;
 import dev.victor.parking.exception.ClientNotFoundException;
+import dev.victor.parking.exception.PatchException;
 import dev.victor.parking.repository.ClientRepository;
 import dev.victor.parking.service.dto.ClientResponseDto;
 import org.springframework.data.domain.Page;
@@ -13,9 +19,11 @@ import org.springframework.stereotype.Service;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final ObjectMapper objectMapper;
 
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, ObjectMapper objectMapper) {
         this.clientRepository = clientRepository;
+        this.objectMapper = objectMapper;
     }
 
     public ClientResponseDto create(ClientRequestDto dto) {
@@ -25,13 +33,28 @@ public class ClientService {
     }
 
     public ClientResponseDto findById(Long id) {
-        Client client = clientRepository.findById(id)
-                .orElseThrow(() -> new ClientNotFoundException(String.format("Client with id: %d not found", id)));
+        Client client = getClientById(id);
         return ClientResponseDto.toDto(client);
     }
 
     public Page<ClientResponseDto> findAll(Pageable pageable) {
         Page<Client> clientsPage = clientRepository.findAll(pageable);
         return clientsPage.map(ClientResponseDto::toDto);
+    }
+
+    public ClientResponseDto updatePartial(Long id, JsonPatch patch) {
+        try {
+            Client client = getClientById(id);
+            JsonNode patched = patch.apply(objectMapper.convertValue(client, JsonNode.class));
+            Client updatedClient = objectMapper.treeToValue(patched, Client.class);
+            return ClientResponseDto.toDto(clientRepository.save(updatedClient));
+        } catch (JsonPatchException | JsonProcessingException e) {
+            throw new PatchException("Failed to apply patch due to invalid document or malformed data");
+        }
+    }
+
+    private Client getClientById(Long id) {
+         return clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException(String.format("Client with id: %d not found", id)));
     }
 }
