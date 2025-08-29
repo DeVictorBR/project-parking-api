fbe2fbebf04707e15d1fe1b766c03f4572afae01
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-29 15:07:48 -0300

    feat(exception-handler): add global handler for request validation errors

--- src/main/java/dev/victor/parking/exception/GlobalHandlerException.java
+++ src/main/java/dev/victor/parking/exception/GlobalHandlerException.java
@@ -1,8 +1,13 @@
 package dev.victor.parking.exception;
 
+import dev.victor.parking.exception.dto.InvalidParamDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
@@ -10,5 +15,18 @@ public class GlobalHandlerException {
     @ExceptionHandler(ParkingException.class)
     public ProblemDetail handleParkingException(ParkingException e) {
         return e.toProblemaDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<InvalidParamDto> invalidParams = e.getFieldErrors()
+                .stream()
+                .map(fe -> new InvalidParamDto(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pd.setTitle("Invalid request parameters");
+        pd.setDetail("There is invalid fields on the request");
+        pd.setProperty("invalid params", invalidParams);
+        return pd;
     }
 }
