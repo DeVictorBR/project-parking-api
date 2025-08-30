26cc43bb63401579a0933d4802626f8a6a89a572
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-30 04:18:38 -0300

    feat(controller): add create vehicle in /vehicles endpoint

---
+++ src/main/java/dev/victor/parking/controller/VehicleController.java
@@ -0,0 +1,36 @@
+package dev.victor.parking.controller;
+
+import dev.victor.parking.controller.dto.VehicleRequestDto;
+import dev.victor.parking.service.VehicleService;
+import dev.victor.parking.service.dto.VehicleResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/vehicles")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<VehicleResponseDto> create(@RequestBody @Valid VehicleRequestDto dto) {
+        VehicleResponseDto responseDto = vehicleService.create(dto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.id())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
+    }
+}
