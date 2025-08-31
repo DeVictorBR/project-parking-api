4efb7dce25a4b6ca49c2aa7086c2f90ab338719c
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 18:26:25 -0300

    feat(service): add method to apply partial update to contract template

--- src/main/java/dev/victor/parking/service/ContractTemplateService.java
+++ src/main/java/dev/victor/parking/service/ContractTemplateService.java
@@ -1,8 +1,14 @@
 package dev.victor.parking.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import dev.victor.parking.controller.dto.ContractTemplateRequestDto;
 import dev.victor.parking.entity.ContractTemplate;
 import dev.victor.parking.exception.ContractTemplateNotFoundException;
+import dev.victor.parking.exception.PatchException;
 import dev.victor.parking.repository.ContractTemplateRepository;
 import dev.victor.parking.service.dto.ContractTemplateResponseDto;
 import org.springframework.data.domain.Page;
@@ -13,9 +19,11 @@ import org.springframework.stereotype.Service;
 public class ContractTemplateService {
 
     private final ContractTemplateRepository contractTemplateRepository;
+    private final ObjectMapper objectMapper;
 
-    public ContractTemplateService(ContractTemplateRepository contractTemplateRepository) {
+    public ContractTemplateService(ContractTemplateRepository contractTemplateRepository, ObjectMapper objectMapper) {
         this.contractTemplateRepository = contractTemplateRepository;
+        this.objectMapper = objectMapper;
     }
 
     public ContractTemplateResponseDto create(ContractTemplateRequestDto dto) {
@@ -32,6 +40,17 @@ public class ContractTemplateService {
     public Page<ContractTemplateResponseDto> findAll(Pageable pageable) {
         Page<ContractTemplate> contractTemplatesPage = contractTemplateRepository.findAll(pageable);
         return contractTemplatesPage.map(ContractTemplateResponseDto::toDto);
+    }
+
+    public ContractTemplateResponseDto updatePartial(Long id, JsonPatch patch) {
+        try {
+            ContractTemplate contractTemplate = getContractTemplateById(id);
+            JsonNode patched = patch.apply(objectMapper.convertValue(contractTemplate, JsonNode.class));
+            ContractTemplate updateContractTemplate = objectMapper.treeToValue(patched, ContractTemplate.class);
+            return ContractTemplateResponseDto.toDto(updateContractTemplate);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            throw new PatchException("Failed to apply patch due to invalid document or malformed data");
+        }
     }
 
     protected ContractTemplate getContractTemplateById(Long id) {
