b5aba46c9278943a1c2c85165c2edc62845f9a2e
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 15:19:04 -0300

    feat(exception): add ContractTemplateNotFoundException

---
+++ src/main/java/dev/victor/parking/exception/ContractTemplateNotFoundException.java
@@ -0,0 +1,22 @@
+package dev.victor.parking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class ContractTemplateNotFoundException extends ParkingException {
+
+    private final String detail;
+
+    public ContractTemplateNotFoundException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemaDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        pd.setTitle("Contract Template not found");
+        pd.setDetail(detail);
+        return pd;
+    }
+}
