3c0cccfe58ed5058485869af2c3f13aaaba6b684
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 02:09:22 -0300

    feat(controller): add /vehicles endpoint to find all vehicles with HATEOAS

--- src/main/java/dev/victor/parking/controller/VehicleController.java
+++ src/main/java/dev/victor/parking/controller/VehicleController.java
@@ -4,11 +4,17 @@ import dev.victor.parking.controller.dto.VehicleRequestDto;
 import dev.victor.parking.service.VehicleService;
 import dev.victor.parking.service.dto.VehicleResponseDto;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/vehicles")
@@ -29,6 +35,28 @@ public class VehicleController {
                 .buildAndExpand(responseDto.id())
                 .toUri();
         return ResponseEntity.created(location).body(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedModel<VehicleResponseDto>> findAll(Pageable pageable) {
+        Page<VehicleResponseDto> vehiclesPage = vehicleService.findAll(pageable);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(
+                vehiclesPage.getSize(),
+                vehiclesPage.getNumber(),
+                vehiclesPage.getTotalElements(),
+                vehiclesPage.getTotalPages()
+        );
+
+        PagedModel<VehicleResponseDto> pagedModel = PagedModel.of(vehiclesPage.getContent(), pageMetadata);
+        pagedModel.add(linkTo(methodOn(VehicleController.class).findAll(pageable)).withSelfRel());
+        if (vehiclesPage.hasPrevious()) {
+            pagedModel.add(linkTo(methodOn(VehicleController.class).findAll(vehiclesPage.previousPageable())).withRel("previous"));
+        }
+        if (vehiclesPage.hasNext()) {
+            pagedModel.add(linkTo(methodOn(VehicleController.class).findAll(vehiclesPage.nextPageable())).withRel("next"));
+        }
+        return ResponseEntity.ok(pagedModel);
     }
 
     @GetMapping(path = "/{id}")
