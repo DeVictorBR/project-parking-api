219dab7d92c29011fe20f185cc9d53c3dfd204e6
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 18:18:59 -0300

    feat(controller): add endpoint /contract-templates to find all contract templates with HATEOAS

--- src/main/java/dev/victor/parking/controller/ContractTemplateController.java
+++ src/main/java/dev/victor/parking/controller/ContractTemplateController.java
@@ -3,11 +3,17 @@ package dev.victor.parking.controller;
 import dev.victor.parking.controller.dto.ContractTemplateRequestDto;
 import dev.victor.parking.service.ContractTemplateService;
 import dev.victor.parking.service.dto.ContractTemplateResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/contract-templates")
@@ -35,5 +41,27 @@ public class ContractTemplateController {
     public ResponseEntity<ContractTemplateResponseDto> findById(@PathVariable Long id) {
         ContractTemplateResponseDto responseDto = contractTemplateService.findById(id);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedModel<ContractTemplateResponseDto>> findAll(Pageable pageable) {
+        Page<ContractTemplateResponseDto> contractTemplatesPage = contractTemplateService.findAll(pageable);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(
+                contractTemplatesPage.getSize(),
+                contractTemplatesPage.getNumber(),
+                contractTemplatesPage.getTotalElements(),
+                contractTemplatesPage.getTotalPages()
+        );
+
+        PagedModel<ContractTemplateResponseDto> pagedModel = PagedModel.of(contractTemplatesPage.getContent(), pageMetadata);
+        pagedModel.add(linkTo(methodOn(ContractTemplateController.class).findAll(pageable)).withSelfRel());
+        if (contractTemplatesPage.hasPrevious()) {
+            pagedModel.add(linkTo(methodOn(ContractTemplateController.class).findAll(contractTemplatesPage.previousPageable())).withRel("previous"));
+        }
+        if (contractTemplatesPage.hasNext()) {
+            pagedModel.add(linkTo(methodOn(ContractTemplateController.class).findAll(contractTemplatesPage.nextPageable())).withRel("next"));
+        }
+        return ResponseEntity.ok(pagedModel);
     }
 }
