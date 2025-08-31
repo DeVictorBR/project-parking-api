cbacf7e6f0f1521c5ab9125074202de8ffbfb612
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 15:19:40 -0300

    feat(service): add method to find contract template by id

--- src/main/java/dev/victor/parking/service/ContractTemplateService.java
+++ src/main/java/dev/victor/parking/service/ContractTemplateService.java
@@ -2,6 +2,7 @@ package dev.victor.parking.service;
 
 import dev.victor.parking.controller.dto.ContractTemplateRequestDto;
 import dev.victor.parking.entity.ContractTemplate;
+import dev.victor.parking.exception.ContractTemplateNotFoundException;
 import dev.victor.parking.repository.ContractTemplateRepository;
 import dev.victor.parking.service.dto.ContractTemplateResponseDto;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,15 @@ public class ContractTemplateService {
         ContractTemplate contractTemplate = dto.toEntity();
         ContractTemplate savedContractTemplate = contractTemplateRepository.save(contractTemplate);
         return ContractTemplateResponseDto.toDto(savedContractTemplate);
+    }
+
+    public ContractTemplateResponseDto findById(Long id) {
+        ContractTemplate contractTemplate = getContractTemplateById(id);
+        return ContractTemplateResponseDto.toDto(contractTemplate);
+    }
+
+    protected ContractTemplate getContractTemplateById(Long id) {
+        return contractTemplateRepository.findById(id)
+                .orElseThrow(() -> new ContractTemplateNotFoundException(String.format("Contract Template with id: %d not found", id)));
     }
 }
