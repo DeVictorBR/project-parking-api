a71909682e2189210b7e746132ce221178fbea38
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-31 15:54:08 -0300

    feat(repository): add methods to find client by CPF and email

--- src/main/java/dev/victor/parking/repository/ClientRepository.java
+++ src/main/java/dev/victor/parking/repository/ClientRepository.java
@@ -3,5 +3,10 @@ package dev.victor.parking.repository;
 import dev.victor.parking.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByCpf(String cpf);
+    Optional<Client> findByEmail(String email);
 }
