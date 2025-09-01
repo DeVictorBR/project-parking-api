0eb7b13505573f9abcb14692b095dbc29dc1e370
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-01 02:40:04 -0300

    feat(repository): add query method to find contracts by status and renewal date

--- src/main/java/dev/victor/parking/repository/ContractRepository.java
+++ src/main/java/dev/victor/parking/repository/ContractRepository.java
@@ -1,7 +1,15 @@
 package dev.victor.parking.repository;
 
 import dev.victor.parking.entity.Contract;
+import dev.victor.parking.entity.enums.ContractStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+    Page<Contract> findByContractStatusAndRenewalDateBetween(
+            ContractStatus status, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable
+    );
 }
