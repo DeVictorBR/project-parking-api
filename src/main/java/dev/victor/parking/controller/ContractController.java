8e159c6ef04fa3cd06507e61353330389a12b7c1
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-01 00:02:59 -0300

    feat(controller): add create contract /contracts endpoint

---
+++ src/main/java/dev/victor/parking/controller/ContractController.java
@@ -0,0 +1,36 @@
+package dev.victor.parking.controller;
+
+import dev.victor.parking.controller.dto.ContractRequestDto;
+import dev.victor.parking.service.ContractService;
+import dev.victor.parking.service.dto.ContractResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/contracts")
+public class ContractController {
+
+    private final ContractService contractService;
+
+    public ContractController(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContractResponseDto> create(@RequestBody @Valid ContractRequestDto dto) {
+        ContractResponseDto responseDto = contractService.create(dto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.id())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
+    }
+}
