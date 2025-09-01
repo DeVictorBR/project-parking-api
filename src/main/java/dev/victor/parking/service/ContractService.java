ae76a4b927a9d1d080eec7c8645ca615a8f8fd58
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-01 01:58:05 -0300

    feat(service): add method to apply partial update to contract

--- src/main/java/dev/victor/parking/service/ContractService.java
+++ src/main/java/dev/victor/parking/service/ContractService.java
@@ -1,5 +1,10 @@
 package dev.victor.parking.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import dev.victor.parking.controller.dto.ContractRequestDto;
 import dev.victor.parking.entity.Contract;
 import dev.victor.parking.entity.ContractTemplate;
@@ -7,6 +12,7 @@ import dev.victor.parking.entity.Vehicle;
 import dev.victor.parking.entity.enums.ContractStatus;
 import dev.victor.parking.exception.ContractAlreadyExistsException;
 import dev.victor.parking.exception.ContractNotFoundException;
+import dev.victor.parking.exception.PatchException;
 import dev.victor.parking.repository.ContractRepository;
 import dev.victor.parking.service.dto.ContractResponseDto;
 import org.springframework.data.domain.Page;
@@ -21,11 +27,13 @@ public class ContractService {
     private final ContractRepository contractRepository;
     private final ContractTemplateService contractTemplateService;
     private final VehicleService vehicleService;
+    private final ObjectMapper objectMapper;
 
-    public ContractService(ContractRepository contractRepository, ContractTemplateService contractTemplateService, VehicleService vehicleService) {
+    public ContractService(ContractRepository contractRepository, ContractTemplateService contractTemplateService, VehicleService vehicleService, ObjectMapper objectMapper) {
         this.contractRepository = contractRepository;
         this.contractTemplateService = contractTemplateService;
         this.vehicleService = vehicleService;
+        this.objectMapper = objectMapper;
     }
 
     public ContractResponseDto create(ContractRequestDto dto) {
@@ -62,6 +70,17 @@ public class ContractService {
     public Page<ContractResponseDto> findAll(Pageable pageable) {
         Page<Contract> contractsPage = contractRepository.findAll(pageable);
         return contractsPage.map(ContractResponseDto::toDto);
+    }
+
+    public ContractResponseDto updatePartial(Long id, JsonPatch patch) {
+        try {
+            Contract contract = getContractById(id);
+            JsonNode patched = patch.apply(objectMapper.convertValue(contract, JsonNode.class));
+            Contract updatedContract = objectMapper.treeToValue(patched, Contract.class);
+            return ContractResponseDto.toDto(contractRepository.save(updatedContract));
+        } catch (JsonPatchException | JsonProcessingException e) {
+            throw new PatchException("Failed to apply patch due to invalid document or malformed data");
+        }
     }
 
     protected Contract getContractById(Long id) {
