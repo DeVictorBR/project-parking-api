dd80e5ee34dc20798ed2de96e9d2ef747d804944
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-24 16:00:37 -0300

    feat(openfeign) add OpenFeign support

--- src/main/java/dev/victor/parking/ParkingApplication.java
+++ src/main/java/dev/victor/parking/ParkingApplication.java
@@ -2,8 +2,10 @@ package dev.victor.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ParkingApplication {
 
 	public static void main(String[] args) {
