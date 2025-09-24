421fadda59be254cfffae754dab0842639fd5591
victor barbosa <victorhenriquebl11@gmail.com>
2025-09-24 16:03:13 -0300

    docs(OpenApi): add description from Create Vehicle endpoint

--- src/main/java/dev/victor/parking/controller/VehicleController.java
+++ src/main/java/dev/victor/parking/controller/VehicleController.java
@@ -4,6 +4,12 @@ import com.github.fge.jsonpatch.JsonPatch;
 import dev.victor.parking.controller.dto.VehicleRequestDto;
 import dev.victor.parking.service.VehicleService;
 import dev.victor.parking.service.dto.VehicleResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +25,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/vehicles")
+@Tag(name = "Vehicles", description = "Management Vehicles")
 public class VehicleController {
 
     private final VehicleService vehicleService;
@@ -28,6 +35,12 @@ public class VehicleController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Vehicle", description = "Creates a new vehicle from a JSON and returns the created vehicle")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Vehicle created successfully",
+            content = @Content(schema = @Schema(implementation = VehicleResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid vehicle data provided")
+    })
     public ResponseEntity<VehicleResponseDto> create(@RequestBody @Valid VehicleRequestDto dto) {
         VehicleResponseDto responseDto = vehicleService.create(dto);
         URI location = ServletUriComponentsBuilder
